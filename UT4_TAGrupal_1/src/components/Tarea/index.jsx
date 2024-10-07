677b49ffd498b82e5008c7736aabc159c7dd1237
Agus1441 <agustin.schlechter@correo.ucu.ed>
2024-10-07 10:02:57 -0300

    Estados

--- UT4_TAGrupal_1/src/components/Tarea/index.jsx
+++ UT4_TAGrupal_1/src/components/Tarea/index.jsx
@@ -19,6 +19,7 @@ const Tarea = ({ task, renderTasks }) => {
       <p className="task-description">Descripción: {task.description}</p>
       <p className="task-assigned">Asignado a: {task.assignedTo}</p>
       <p className="task-priority">Prioridad: {task.priority}</p>
+      <p className="task-state">Estado {task.state}</p>
       <p className="task-due-date">Fecha límite: {task.endDate}</p>
       <button className="deleteButton" onClick={handleDelete}></button>
     </div>
